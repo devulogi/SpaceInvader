83e8f95ef07754efb2fde7a56cf45d196587c9b5
devulogi <robertdpalmares@gmail.com>
2020-10-21 21:29:19 +0800

    sec

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const favicon = require("serve-favicon");
 const path = require("path");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(function (req, res) {
-  res.render("index");
+  res.status(200).render("index");
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
